598ab8a354a278fa6a6da17866930ef13f5266e9
guil <guil1101@outlook.com>
2023-04-11 15:51:52 -0300

    operacoees

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,6 +6,14 @@ function preenchervisor(num) {
     visor.append(selectedNumber);
   }
   
+function calcular() {
+  const operacoes = window.operationsArray.join("");
+  const resultado = eval(operacoes);
+  window.operationsArray = [resultado];
+  const visor = document.querySelector(".visor");
+  visor.innerHTML = resultado;
+}
+
   const createNumber = (num) => {
     const div = document.createElement("div");
     div.className = "number";
@@ -38,8 +46,21 @@ function preenchervisor(num) {
   
       lin.prepend(btn);
     }
-  };
   
+  const btnMais = createNumber("+");
+  const btnMenos = createNumber("-");
+  const btnVezes = createNumber("*");
+  const btnDividir = createNumber("/");
+  divNumbers.appendChild(btnMais);
+  divNumbers.appendChild(btnMenos);
+  divNumbers.appendChild(btnVezes);
+  divNumbers.appendChild(btnDividir);
+
+  const btnIgual = createNumber("=");
+  btnIgual.onclick = calcular;
+  divNumbers.appendChild(btnIgual);
+  };
+
   const createVisor = (reference) => {
     const divCalculadora = document.getElementById(reference);
     let visor = document.createElement("div");
